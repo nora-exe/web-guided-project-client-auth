cdbc1b7a293d320641133168128137add3dbf271
Dustin Myers <dustint314@gmail.com>
2021-04-20 11:36:29 -0600

    wip

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+// render a Route component
+// take in all of the same props as a regular Route comp
+// check to see if
+
+function PrivateRoute() {
+  return <Route />;
+}
+
+export default PrivateRoute;
