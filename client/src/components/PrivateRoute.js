914ebec2bab4a55a7f70cc04af740166b2afb1c6
Dustin Myers <dustint314@gmail.com>
2021-04-20 11:39:34 -0600

    wip

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 
 // render a Route component
 // take in all of the same props as a regular Route comp
-// check to see if
+// check to see if the user is authenticated
+// if they are, render the component that was passed in
+// if not, redirect user to "/login"
 
 function PrivateRoute(props) {
-  return <Route {...props} />;
+  return (
+    <Route
+      {...props}
+      render={() => {
+        if (window.localStorage.getItem("token")) {
+          return; // the component passed in through props
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+  );
 }
 
 export default PrivateRoute;
