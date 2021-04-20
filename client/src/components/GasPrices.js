a552563c90ae10d30c060831730203778199e4af
Dustin Myers <dustint314@gmail.com>
2021-04-20 10:53:43 -0600

    wip

--- client/src/components/GasPrices.js
+++ client/src/components/GasPrices.js
@@ -1,6 +1,7 @@
 import React from "react";
 import moment from "moment";
 import Loader from "react-loader-spinner";
+import axios from "axios";
 
 class GasPrices extends React.Component {
   state = {
@@ -15,7 +16,7 @@ class GasPrices extends React.Component {
     axios
       .get("http://localhost:5000/api/data")
       .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log({ err }));
   };
 
   formatData = () => {
